433dad2e4fbcfb96b6c21d04d46a4d91974e79a9
yanminli <lym_ay@sina.com>
2018-04-12 17:19:10 +0800

    add new program
    add new program

---
+++ Doll.java
@@ -0,0 +1,10 @@
+public class Doll{
+    private String name;
+    public Doll(String name){
+        this.name = name;
+    }
+
+    public void speak(){
+        System.out.println(name);
+    }
+}
